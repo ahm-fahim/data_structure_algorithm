3229ff99be6269e9190acfe3b5571ba2fb481328
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-03 16:09:32 +0600

    linear search

---
+++ Searching Algorithm/Linear Search/example1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int find_value = 9;
+    int position = 0;
+    int count = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr[i] == find_value)
+        {
+            position = position + i;
+            count += 1;
+            break;
+        }
+    }
+
+    if (count != 0)
+    {
+        printf("%d is fiound on %dth index\n", find_value, position);
+    }
+    else
+    {
+        printf("Not Found\n");
+    }
+    return 0;
+}
