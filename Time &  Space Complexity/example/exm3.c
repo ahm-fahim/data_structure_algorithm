0f7ab50322d4a474854b9ff411aed01136fa596e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-03 08:17:58 +0600

    time & space complexity exm 3

--- Time &  Space Complexity/example/exm3.c
+++ Time &  Space Complexity/example/exm3.c
@@ -1,7 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int i, j, n, count;
 
-    scanf()
+    scanf("%d", &n);
+
+    count = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            count = count + 1;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        count = count + 1;
+    }
+
+    printf("count= %d\n", count);
+
+    return 0;
 }
+// time complexity 0(n*2)
