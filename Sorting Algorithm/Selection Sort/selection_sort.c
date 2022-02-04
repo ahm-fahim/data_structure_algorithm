ad350b365a84d1c0235b7a4541bb2f0344989e25
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-04 16:17:16 +0600

    selection sort

---
+++ Sorting Algorithm/Selection Sort/selection_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void selectionSort(int A[], int n)
+{
+    int i, j, index_min, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        index_min = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (A[j] < A[index_min])
+            {
+                index_min = j;
+            }
+        }
+        if (index_min != i)
+        {
+            // A[i] ও A[index_min] অদল বদল করতে হবে
+            temp = A[i];
+            A[i] = A[index_min];
+            A[index_min] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {8, 2, 4, 1, 5, 7};
+    int n = 6;
+    selectionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
